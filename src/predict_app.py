8e32ab3c886d363f0733b7175511927b1308d241
snezhanadude <snezhanadudkina@gmail.com>
2024-06-04 17:17:40 +0300

    update README.md

---
+++ src/predict_app.py
@@ -0,0 +1,56 @@
+"""House price prediction service"""
+from dotenv import dotenv_values
+from flask import Flask, request
+from flask_cors import CORS
+from joblib import load
+from flask_httpauth import HTTPTokenAuth
+
+
+MODEL_SAVE_PATH = 'models/linear_regression_v01.joblib'
+
+app = Flask(__name__)
+CORS(app)
+
+config = dotenv_values(".env")
+auth = HTTPTokenAuth(scheme='Bearer')
+
+tokens = {
+    config['APP_TOKEN']: "user1",
+}
+
+model = load(MODEL_SAVE_PATH)
+
+
+@auth.verify_token
+def verify_token(token):
+    if token in tokens:
+        return tokens[token]
+
+
+def predict(in_data: dict) -> int:
+    """ Predict house price from input data parameters.
+    :param in_data: house parameters.
+    :raise Error: If something goes wrong.
+    :return: House price, RUB.
+    :rtype: int
+    """
+    area = float(in_data['area'])
+    price = model.predict([[area]])
+    return int(price)
+
+
+@app.route("/")
+def home():
+    return '<h1>Housing price service.</h1> Use /predict endpoint'
+
+
+@app.route("/predict", methods=['POST'])
+@auth.login_required
+def predict_web_serve():
+    """Dummy service"""
+    in_data = request.get_json()
+    price = predict(in_data)
+    return {'price': price}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
